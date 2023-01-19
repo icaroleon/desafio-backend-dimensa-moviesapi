65f695065a52f2eb69f7c4276220f02384f5c456
Icaro Leon <icvieiramg@gmail.com>
2023-01-18 21:28:02 -0300

    create implementado. ainda falta a questão referente a geração de uma unique id

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_01_18_000127) do
+ActiveRecord::Schema.define(version: 2023_01_18_000801) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -18,6 +18,12 @@ ActiveRecord::Schema.define(version: 2023_01_18_000127) do
   create_table "movies", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "title"
+    t.string "genre"
+    t.integer "year"
+    t.string "country"
+    t.text "description"
+    t.date "published_at"
   end
 
 end
