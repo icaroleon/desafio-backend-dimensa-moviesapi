4fee492528ead8c9ea92e6ed13e651767b2c0461
Icaro Leon <icvieiramg@gmail.com>
2023-01-18 21:58:26 -0300

    controller ok

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -11,7 +11,6 @@ class MoviesController < ActionController::API
     new_id = SecureRandom.uuid
     
     if csv_file.nil?
-<<<<<<< HEAD
       return render :json => { :error => "We don't receive an CSV file. Please check again." }, :status => 200
     elsif csv_file.content_type != 'text/csv'
       return render :json => { :error => 'We only accept CSV files. Please check again.' }, :status => 415
@@ -26,17 +25,6 @@ class MoviesController < ActionController::API
           description: row['description'] })
         movie.save
         return render :json => { :successful => 'We just saved the CSV that you send to us. Thank you.' }, :status => 200
-=======
-      render :json => { :error => "We don't receive a CSV file. Please check again." }, :status => 204
-    elsif csv_file.content_type != 'text/csv'
-      render :json => { :error => 'We only accept CSV files. Please check again.' }, :status => 415
-    else  
-      CSV.foreach(csv_file, headers: true) do |row|
-        data = row.to_hash
-        ActiveRecord::Base.transaction do
-          Movie.create(data)
-        end
->>>>>>> 289ba9c96ad6475b7d794e8f85ad1f3dfb3ec017
       end
     end  
   end
