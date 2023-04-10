8f3ea71523372e0e3226bd13e9149e97c9274f39
Icaro Leon <icvieiramg@gmail.com>
2023-04-10 14:03:49 -0300

    routes ok

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,6 @@ Rails.application.routes.draw do
   namespace :api, defaults: { format: :json} do
     namespace :v1 do
       get "/movies/query", to: "movies#index" 
-      get '/movies/page/:page', to: "movies#index", as: ''
 
     resources :movies, only: %i[index create]
     end
